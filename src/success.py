700373942c7a7e5b674bd26c0385f8a5907fc926
Tamat Grinboim <tamar7778@gmail.com>
2026-02-23 12:02:04 +0200

    change jenkinsFile

---
+++ src/success.py
@@ -0,0 +1,2 @@
+def print_message():
+    print("good luck")
--- test/test_hello.py
+++ test/test_hello.py
@@ -1,7 +1,11 @@
-import pytest
-from  src.hello import print_hello
+from src.hello import print_hello
+
+from src.success import print_message
+
 
 def test_hello():
-    assert print_hello=="hello shira"
+    assert print_hello == "hello shira"
 
 
+def test_hello():
+    assert print_message == "good luck shira and varda"
